b9a657d2a8467eb0fcd7094e2985cb3d64bf04de
rohitdhore <rohit.dhore1612@gmail.com>
2016-10-14 23:35:31 +0530

    initial commit for clf app

---
+++ app/scripts/app.js
@@ -0,0 +1,33 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name clfApp
+ * @description
+ * # clfApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('clfApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
