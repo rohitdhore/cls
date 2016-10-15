c021ce06c648141efb8a2abd9a488187a42f46c8
rohitdhore <rohit.dhore1612@gmail.com>
2016-10-16 00:38:07 +0530

    home page partial

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -14,4 +14,16 @@ angular.module('clfApp')
       'AngularJS',
       'Karma'
     ];
+
+    $scope.categories = [
+    {id:1, name: 'mobiles', title: 'Mobiles'},
+    {id:2, name: 'electronics', title: 'Electronics & Appliances'},
+    {id:3, name: 'cars', title: 'Cars'},
+    {id:4, name: 'bikes', title: 'Bikes'},
+    {id:5, name: 'furniture', title: 'Furniture'},
+    {id:6, name: 'books', title: 'Books, Sports & Hobbies'},
+    {id:7, name: 'fashion', title: 'Fashion'},
+    {id:8, name: 'pets', title: 'Pets'},
+    {id:9, name: 'realestate', title: 'Real Estate'},
+    {id:10, name: 'jobs', title: 'Jobs & Services'}]
   });
